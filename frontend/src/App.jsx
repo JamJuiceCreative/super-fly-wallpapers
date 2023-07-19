2a9928385fb12f7366b80734e64cd1796734ea32
JamJuiceCreative <jim@jamjuicecreative.com.au>
2023-07-20 09:01:47 +1000

    front end styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
-  )
-}
+    <div className="App">
+      <header className="App-header">
+        <div className="flex justify-center items-center h-120 w-screen">
+          <a href="/">
+            <img
+              src="src/assets/SuperFlyWallPapersLogo.svg"
+              className="w-3/4 sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mx-auto"
+            />
+          </a>
+        </div>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+};
 
-export default App
+export default App;
