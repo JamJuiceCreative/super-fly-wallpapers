286ef3b0d319d8b4c721812a7909576ba97070aa
JamJuiceCreative <jim@jamjuicecreative.com.au>
2023-07-20 10:51:15 +1000

    set up designs objets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import data from './data';
 
 const App = () => {
   return (
@@ -7,13 +8,29 @@ const App = () => {
         <div className="flex justify-center items-center h-120 w-screen">
           <a href="/">
             <img
-              src="src/assets/SuperFlyWallPapersLogo.svg"
-              className="w-3/4 sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mx-auto"
+              src="/images/SuperFlyWallPapersLogo.svg"
+              className="w-3/4 sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mx-auto"alt="superfly wallpapers logo"
             />
           </a>
         </div>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="designs">
+          {data.designs.map((design) => (
+            <div className="design" key={design.slug}>
+              <a href={`/design/${design.slug}`}>
+                <img src={design.image} alt={design.name} />
+              </a>
+              <div className="product-info">
+                <p>{design.name} </p>
+                <p>{design.price} </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 };
