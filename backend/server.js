d80001029d9a396490116371aacbacb2fb5da2a4
JamJuiceCreative <jim@jamjuicecreative.com.au>
2023-07-25 20:17:21 +1000

    getting ready to merge to main and deploy

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import data from './data.js';
+import path from 'path';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -48,6 +48,12 @@ app.use('/api/seed', seedRouter);
 app.use('/api/designs', designRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
